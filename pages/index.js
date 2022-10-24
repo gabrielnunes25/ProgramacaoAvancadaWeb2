87ccf7df719723784e15396d583866d7392cd887
gabrielnunes25 <gabrielns2503@gmail.com>
2022-10-24 20:47:24 -0300

    24 de out

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,50 +11,58 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Pedidos
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.card}>
+            <h2>Nome do cliente</h2>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <table>
+              <tr>
+                <th>Quantidade</th>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Total</th>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Caneta</td>
+                <td>R$ 2,00</td>
+                <td>R$ 6,00</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Lapis</td>
+                <td>R$ 1,00</td>
+                <td>R$ 2,00</td>
+              </tr>
+              <tr>
+                
+                <td colSpan={3}>Total:</td>
+                <td>R$ 8,00</td>
+              </tr>
+              
+            </table>
+          </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <h2>Nome do cliente 2</h2>
+
+            <table>
+
+            </table>
+          </div>
+
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
           href=""
-          // target="_blank"
-          // rel="noopener noreferrer"
+        // target="_blank"
+        // rel="noopener noreferrer"
         >
           Gabriel Nunes Silva
         </a>
